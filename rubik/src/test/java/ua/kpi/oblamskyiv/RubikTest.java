955f303bbfb5da687b377fe566226024045b54f8
OblamskyiV <vlad.oblamsky@gmail.com>
2013-03-23 11:53:07 +0200

    Test cases

---
+++ rubik/src/test/java/ua/kpi/oblamskyiv/RubikTest.java
@@ -0,0 +1,17 @@
+package ua.kpi.oblamskyiv;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oblamskyiv
+ * Date: 3/23/13
+ * Time: 11:47 AM
+ */
+public class RubikTest {
+
+    // Появляється кубик
+    // Кубик розміром 3х3х3
+    // Кубик випадково перемішується
+    // Можна повертати всі грані
+    // Коли кожна грань буде однакового кольору - кінець гри
+
+}
